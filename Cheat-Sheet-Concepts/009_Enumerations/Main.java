42477483881f2ba5a4058d8ad99082166fecf9a3
Jar <jagaradoz@gmail.com>
2024-11-23 11:15:30 +0700

    Add A Cheat Sheet Concept

---
+++ Cheat-Sheet-Concepts/009_Enumerations/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        TestValue testValue = TestValue.VALUE_1;
+        Animal animal = Animal.CAT;
+        Car car = Car.SUV;
+    }
+}
+
+
+
